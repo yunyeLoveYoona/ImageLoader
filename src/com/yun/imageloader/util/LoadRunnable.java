d032a5157f130959163c587de74be03352c03f89
yunye <550752356@qq.com>
2015-10-29 14:14:45 +0800

    Update LoadRunnable.java

--- src/com/yun/imageloader/util/LoadRunnable.java
+++ src/com/yun/imageloader/util/LoadRunnable.java
@@ -7,8 +7,10 @@ import android.os.Handler;
 import android.os.Message;
 import android.widget.ImageView;
 
+import com.yun.imagescan.NativeImageLoader;
+
 /**
- * Í¼Æ¬»ñÈ¡Ïß³Ì
+ * å›¾ç‰‡èŽ·å–çº¿ç¨‹
  * 
  * @author yunye
  * 
@@ -22,6 +24,7 @@ public class LoadRunnable implements Runnable {
 	private boolean isCache;
 	private Handler handler;
 	private Drawable errorImage;
+	private Bitmap bitmap;
 
 	public LoadRunnable(ImageView imageView, String url, int defaultBackround,
 			Drawable errorImage, int width, int height, boolean isCache,
@@ -39,43 +42,55 @@ public class LoadRunnable implements Runnable {
 	@Override
 	public void run() {
 		if (url.contains("http://")) {
-			// ÍøÂçÍ¼Æ¬
-			final Bitmap bitmap = new ImageDownLoad().downLoad(url, width,
-					height);
-			if (bitmap != null) {
-				ImageCache.getInstance().put(url, bitmap);
-				if (isCache) {
-					Message message = handler.obtainMessage();
-					message.obj = url;
-					handler.sendMessage(message);
-				}
-			}
-			handler.post(new Runnable() {
-				@Override
-				public void run() {
-					if (bitmap == null && errorImage != null) {
-						imageView.setImageDrawable(errorImage);
-					} else {
-						imageView.setImageBitmap(bitmap);
+			// ç½‘ç»œå›¾ç‰‡
+			try {
+				final Bitmap bitmap = new ImageDownLoad().downLoad(url, width,
+						height);
+				if (bitmap != null) {
+					NativeImageLoader.getInstance().addBitmapToMemoryCache(url,
+							bitmap);
+					if (isCache) {
+						Message message = handler.obtainMessage();
+						message.obj = url;
+						handler.sendMessage(message);
 					}
-
 				}
-			});
+				handler.post(new Runnable() {
+					@Override
+					public void run() {
+						if (bitmap == null && errorImage != null) {
+							imageView.setImageDrawable(errorImage);
+							imageView = null;
+							errorImage = null;
+						} else {
+							imageView.setImageBitmap(bitmap);
+							imageView = null;
+							errorImage = null;
+						}
+
+					}
+				});
+			} catch (Exception e) {
+			}
+
 		} else {
-			// ±¾µØÍ¼Æ¬
+			// æœ¬åœ°å›¾ç‰‡
 			BitmapFactory.Options options = new BitmapFactory.Options();
 			options.inJustDecodeBounds = true;
 			BitmapFactory.decodeFile(url, options);
-			options.inSampleSize = width / options.outWidth >= height
-					/ options.outHeight ? width / options.outWidth
-					: options.outHeight;
+			options.inSampleSize = options.outWidth / width >= options.outHeight
+					/ height ? options.outWidth / width : options.outHeight
+					/ height;
 			options.inJustDecodeBounds = false;
-			final Bitmap bitmap = BitmapFactory.decodeFile(url, options);
-			ImageCache.getInstance().put(url, bitmap);
+			bitmap = BitmapFactory.decodeFile(url, options);
+			NativeImageLoader.getInstance().addBitmapToMemoryCache(url, bitmap);
 			handler.post(new Runnable() {
 				@Override
 				public void run() {
 					imageView.setImageBitmap(bitmap);
+					imageView = null;
+					errorImage = null;
+					bitmap = null;
 				}
 			});
 		}
